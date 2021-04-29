e1a6cf353ffc4923b114a51dca482d5f41f2425b
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-04-29 05:31:53 +0100

    fixed

--- ConsoleSnake/Program.cs
+++ ConsoleSnake/Program.cs
@@ -147,7 +147,7 @@ namespace ConsoleSnake
             }
 
             // check if out side bounds of map or we have hit our own tail\other part of snake
-            if (point.X >= _display.Length || point.Y >= _display[0].Length || point.X < 0 || point.Y < 1 ||
+            if (point.X >= _display.Length || point.Y >= _display[0].Length || point.X < 0 || point.Y < 0 ||
                 _snake.Skip(1).Contains(point))
             {
                 Console.Clear();
